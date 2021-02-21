dc3b3e3d0233509399d342d9ccd5105198570640
Owen Sullivan <multiojuice@gmail.com>
2021-02-21 00:38:31 -0500

    Commuicating input sends and changes via the sockets

--- src/index.js
+++ src/index.js
@@ -10,9 +10,8 @@ const io = require('socket.io')(http, {
 });
 
 const games = {
-  hi: "tanks"
+  
 }
-
 
 app.disable('etag');
 app.use(cors());
@@ -22,7 +21,7 @@ app.get('/create/:game_id', (req,res) => {
 
   const {game_id} = req.params;
   games[session_id] = {
-    game_id: 'maze'
+    game_id: game_id
   };
   console.warn(`creating ${session_id}`)
   res.json({session_id});
@@ -38,15 +37,25 @@ io.on('connection', (socket) => {
     if (!games[session_id]) games[session_id] = {};
     
     games[session_id].input_type = starting_input;
-    socket.join(`session-${session_id}`)
+    socket.join(`host-${session_id}`)
   });
+
+  socket.on('change-input', ({session_id, input_type}) => {
+    games[session_id].input_type = input_type;
+    socket.to(`players-${session_id}`).emit('change-input', input_type);
+  })
+
+  socket.on('send-input', ({session_id, input_data}) => {
+    socket.to(`host-${session_id}`).emit('send-input', input_data, socket.id);
+  })
 
   socket.on('join', ({session_id}) => {
     console.warn(`Player joined ${session_id}`)
     if (!games[session_id]) {
       socket.emit('error', true);
     } else {
-      socket.join(`session-${session_id}`)
+      socket.join(`players-${session_id}`)
+      socket.to(`host-${session_id}`).emit('create-player', socket.id)
       socket.emit('change-input', games[session_id].input_type);
     }
   });
