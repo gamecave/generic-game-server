106ddd0f15287fc4f14aea9bc73b9edd6b6d7357
Owen Sullivan <multiojuice@gmail.com>
2021-02-21 00:17:12 -0500

    Completed room joining and input changing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const app = require('express')();
 const http = require('http').Server(app);
-var cors = require('cors')
+const randomize = require('randomatic');
+const cors = require('cors')
 const io = require('socket.io')(http, {
   cors: {
     origin: "http://localhost:8080",
@@ -12,13 +13,19 @@ const games = {
   hi: "tanks"
 }
 
+
+app.disable('etag');
 app.use(cors());
 
-app.get('/create/:id', (req,res) => {
-  console.warn(games);
-  games.TSCS = 'maze';
-  
-  res.send({sessionId: 'TSCS'});
+app.get('/create/:game_id', (req,res) => {
+  const session_id = randomize('A', 5);
+
+  const {game_id} = req.params;
+  games[session_id] = {
+    game_id: 'maze'
+  };
+  console.warn(`creating ${session_id}`)
+  res.json({session_id});
 });
 
 app.get('/', (req, res) => {
@@ -26,8 +33,22 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  socket.on('host', data => {
-    console.log('host', data);
+  socket.on('host', ({session_id, starting_input}) => {
+    console.warn(`Host connected to: ${session_id}`)
+    if (!games[session_id]) games[session_id] = {};
+    
+    games[session_id].input_type = starting_input;
+    socket.join(`session-${session_id}`)
+  });
+
+  socket.on('join', ({session_id}) => {
+    console.warn(`Player joined ${session_id}`)
+    if (!games[session_id]) {
+      socket.emit('error', true);
+    } else {
+      socket.join(`session-${session_id}`)
+      socket.emit('change-input', games[session_id].input_type);
+    }
   });
 });
 
@@ -35,4 +56,3 @@ io.on('connection', (socket) => {
 http.listen(3000, () => {
   console.log('listening on *:3000');
 });
-
